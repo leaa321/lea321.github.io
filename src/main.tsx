24d93089623ce6649bbba457b8dd4fe2b25b5bf1
Lea Drewes <lea.drewes@hec.de>
2026-02-25 10:38:06 +0100

    feat added font variables

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./index.scss";
 import { MyRouter } from "./pages/MyRouter";
 
 createRoot(document.getElementById("root")!).render(
