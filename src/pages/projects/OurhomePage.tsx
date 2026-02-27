9abb9b06b652acef810a70f5de9b990b3d843ed6
Lea Drewes <lea.drewes@hec.de>
2026-02-27 09:31:13 +0100

    styled icons in feaure

--- src/pages/projects/OurhomePage.tsx
+++ src/pages/projects/OurhomePage.tsx
@@ -89,7 +89,7 @@ export function OurHomePage() {
           <img className="project-image-item" loading="lazy" src={ourHomeTaskPage} alt="ourHome task page" />
           {/* ADD GIF HERE for categories*/}
           <p className="card-description ">
-            Tasks can be sorted by categories to keep things clear and structured. The app comes with a few default categories, but you can create your own and assign a color to each one for easy recognition. Tasks can also be grouped by how often they repeat: daily, weekly, or monthly. <br /> <br /> When a recurring task is completed, it’s marked as done and removed from the active list until its next due time, so a weekly task like vacuuming only shows up again the following week.
+            Tasks can be sorted by categories to keep things clear and structured. The app comes with a few default categories, but you can create your own and assign a color or icon to each one for easy recognition. Tasks can also be grouped by how often they repeat: daily, weekly, or monthly. <br /> <br /> When a recurring task is completed, it’s marked as done and removed from the active list until its next due time, so a weekly task like vacuuming only shows up again the following week.
           </p>
         </div>
       </div>
