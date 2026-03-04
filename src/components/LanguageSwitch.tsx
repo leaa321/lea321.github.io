d6cf033cd5c3c0ed566f569dc8ad7f6752253890
Lea Drewes <lea.drewes@hec.de>
2026-03-04 09:15:04 +0100

    feat added language switch

--- src/components/LanguageSwitch.tsx
+++ src/components/LanguageSwitch.tsx
@@ -1,18 +1,27 @@
-import i18next from "i18next"
+import { useTranslation } from "react-i18next";
 
+export function LanguageSwitch() {
+    const { i18n } = useTranslation();
+    const activeLanguage = (i18n.resolvedLanguage ?? i18n.resolvedLanguage ?? "en").toLowerCase().startsWith("de") ? "de" : "en";
 
-export function LanguageSwitch{
-    const activeLanguage = (i18next.resolvedLanguage ?? i18next.resolvedLanguage ?? "en").toLowerCase().startsWith("de") ? "de" : "en";
+    const setLanguage = (language: "en" | "de") => {
+        if (activeLanguage !== language) {
+            i18n.changeLanguage(language);
+        }
+    }
 
     function toggleLanguage() {
         const newLanguage = activeLanguage === "en" ? "de" : "en";
+        console.log(newLanguage);
+        document.documentElement.lang = newLanguage;
+        setLanguage(newLanguage);
     }
 
     return (
         <>
-            <button onClick={toggleLanguage()}>
+            <button onClick={toggleLanguage}>
 
             </button>
         </>
-    )
+    );
 }
